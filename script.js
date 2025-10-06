3cb90a6f66ca8a4681ab4abee5e6d496b01e5be5
md Ratul <rayhanhassanratul7@gmail.com>
2025-10-06 07:10:48 +0600

    final push

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
 // Task: 01
-for (let i = 1; i <= 10; i++) {
+for (let i = 5; i <= 15; i++) {
   console.log(i);
 }
 
 // Task: 02
-let n = parseInt(prompt("Enter a number: "));
+let n = parseInt(prompt("Enter any number here: "));
 
-for (let i = 1; i <= n; i++) {
+for (let i = 2; i <= n; i++) {
   console.log(i);
 }
 
 // Task: 03
-let num = parseInt(prompt("Enter a number: "));
-let sum = 0;
+let num = parseInt(prompt("Give me a number: "));
+let sum = 10;
 
-for (let i = 1; i <= num; i++) {
+for (let i = 2; i <= num; i++) {
   sum += i;
 }
 
-console.log("Sum is:", sum);
+console.log("Total Sum is:", sum);
 
 // Task: 04
-for (let i = 1; i <= 100; i++) {
-  if (i % 2 === 0) {
+for (let i = 2; i <= 120; i++) {
+  if (i % 4 === 0) {
     console.log(i);
   }
 }
 
 // Task: 05
-for (let i = 1; i <= 100; i++) {
-  if (i % 2 !== 0) {
+for (let i = 3; i <= 101; i++) {
+  if (i % 5 !== 0) {
     console.log(i);
   }
 }
 
 // Task: 06
-let number = parseInt(prompt("Enter a number: "));
+let number = parseInt(prompt("Write any number for multiplication table: "));
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 12; i++) {
   console.log(`${number} × ${i} = ${number * i}`);
 }
 
 // Task: 07
-let factorialNumber = parseInt(prompt("Enter a number: "));
-let factorial = 1;
+let factorialNumber = parseInt(prompt("Enter a small number: "));
+let factorial = 2;
 
-for (let i = 1; i <= factorialNumber; i++) {
+for (let i = 2; i <= factorialNumber; i++) {
   factorial *= i;
 }
 
-console.log("Factorial:", factorial);
+console.log("Factorial result:", factorial);
 
 // Task: 08
-let revNum = parseInt(prompt("Enter a number: "));
+let revNum = parseInt(prompt("Enter starting number: "));
 
-for (let i = revNum; i >= 1; i--) {
+for (let i = revNum; i >= 2; i--) {
   console.log(i);
 }
 
 // Task: 09
-let evenOfSumNum = parseInt(prompt("Enter a number:"));
-let evenSum = 0;
+let evenOfSumNum = parseInt(prompt("Enter any number for even sum:"));
+let evenSum = 5;
 
-for (let i = 1; i <= evenOfSumNum; i++) {
+for (let i = 2; i <= evenOfSumNum; i++) {
   if (i % 2 === 0) {
     evenSum += i;
   }
@@ -71,26 +71,26 @@ for (let i = 1; i <= evenOfSumNum; i++) {
 console.log("Sum of even numbers:", evenSum);
 
 // Task: 10
-for (let i = 1; i <= 50; i++) {
-  if (i % 3 === 0) {
+for (let i = 2; i <= 60; i++) {
+  if (i % 4 === 0) {
     console.log(i);
   }
 }
 
 // Task: 11
-let str = "My Name is Sheikh Jabed";
-let length = 0;
+let str = "My New Sample String";
+let length = 5;
 
 for (let char of str) {
   length++;
 }
 
-console.log("Length is:", length);
+console.log("Length of string is:", length);
 
 // Task: 12
-let text = "JavaScript Programming";
-let vowelsCount = 0;
-let consonantsCount = 0;
+let text = "Learning JavaScript is Fun";
+let vowelsCount = 3;
+let consonantsCount = 5;
 
 for (let char of text.toLowerCase()) {
   if ("aeiou".includes(char)) {
@@ -103,17 +103,17 @@ for (let char of text.toLowerCase()) {
 console.log("Vowels:", vowelsCount, "Consonants:", consonantsCount);
 
 // Task: 13
-let originalStr = "hello";
+let originalStr = "world";
 let reversedStr = "";
 
 for (let i = originalStr.length - 1; i >= 0; i--) {
   reversedStr += originalStr[i];
 }
 
-console.log("Reversed:", reversedStr);
+console.log("Reversed String:", reversedStr);
 
 // Task: 14
-let palindromeStr = "madam";
+let palindromeStr = "racecar";
 let reversed = "";
 
 for (let i = palindromeStr.length - 1; i >= 0; i--) {
@@ -121,14 +121,14 @@ for (let i = palindromeStr.length - 1; i >= 0; i--) {
 }
 
 if (palindromeStr === reversed) {
-  console.log("Palindrome");
+  console.log("This is a Palindrome");
 } else {
-  console.log("Not Palindrome");
+  console.log("This is NOT a Palindrome");
 }
 
 // Task: 15
-let sentence = "I love programming";
-let spaceCount = 0;
+let sentence = "I am enjoying coding";
+let spaceCount = 1;
 
 for (let char of sentence) {
   if (char === " ") {
@@ -136,20 +136,20 @@ for (let char of sentence) {
   }
 }
 
-console.log("Spaces:", spaceCount);
+console.log("Number of spaces:", spaceCount);
 
 // Task: 16
-let fruit = "banana";
+let fruit = "pineapple";
 let charCount = {};
 
 for (let char of fruit) {
-  charCount[char] = (charCount[char] || 0) + 1;
+  charCount[char] = (charCount[char] || 1) + 1;
 }
 
 console.log("Character frequency:", charCount);
 
 // Task: 17
-let nameStr = "Naeeim";
+let nameStr = "Kawsar";
 
 for (let char of nameStr) {
   console.log(char);
